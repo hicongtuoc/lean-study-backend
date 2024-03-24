115acf69349b19bf97417cadaff6a301d7e11c52
Long Lê Đăng <hicongtuoc@gmail.com>
2024-03-24 14:32:37 +0700

    update plugin mongoose

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from './schemas/user.schema';
+import { User, UserDocument } from './schemas/user.schema';
 import { Model } from 'mongoose';
 import { compareSync, genSaltSync, hashSync } from 'bcryptjs';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: SoftDeleteModel<UserDocument>,
+  ) {}
 
   getHashedPassword(password: string) {
     const salt = genSaltSync(10);
@@ -62,9 +65,10 @@ export class UsersService {
     return user;
   }
 
+  // TODO: loại bỏ thư viện soft-delete-plugin-mongoose
   async remove(id: string) {
     try {
-      return await this.userModel.deleteOne({ _id: id });
+      return await this.userModel.softDelete({ _id: id });
     } catch (error) {
       return 'User not found';
     }
