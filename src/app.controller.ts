8d7176fae84e91a2a16b0a42db5caeffc230c036
Long Lê Đăng <hicongtuoc@gmail.com>
2024-03-25 00:36:08 +0700

    create companies and update user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,6 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { Public } from './decorator/customize';
 
 @Controller()
 export class AppController {
@@ -15,17 +12,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Public()
-  @UseGuards(LocalAuthGuard)
-  @Post('/login')
-  handleLogin(@Request() req) {
-    return this.authService.login(req.user);
-  }
-
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
   }
 }
