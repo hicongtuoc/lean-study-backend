f393a36711dc00639449766f0576878025b20991
Long Lê Đăng <hicongtuoc@gmail.com>
2024-03-30 01:01:47 +0700

    update module permission

--- src/permissions/permissions.service.ts
+++ src/permissions/permissions.service.ts
@@ -1,26 +1,114 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { IUser } from 'src/users/users.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Permission, PermissionDocument } from './schemas/permission.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class PermissionsService {
-  create(createPermissionDto: CreatePermissionDto) {
-    return 'This action adds a new permission';
+  constructor(
+    @InjectModel(Permission.name)
+    private permissionModel: SoftDeleteModel<PermissionDocument>,
+  ) {}
+  async create(createPermissionDto: CreatePermissionDto, user: IUser) {
+    const isExist = await this.permissionModel.findOne({
+      apiPath: createPermissionDto.apiPath,
+      method: createPermissionDto.method,
+    });
+    if (isExist) {
+      throw new BadRequestException('Permission already exists');
+    }
+
+    const newPermission = await this.permissionModel.create({
+      ...createPermissionDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
+    });
+
+    return {
+      _id: newPermission?._id,
+      createdAt: newPermission?.createdAt,
+    };
   }
 
-  findAll() {
-    return `This action returns all permissions`;
+  async findAll(currentPage: number, limit: number, query: string) {
+    const { filter, sort, population } = aqp(query);
+    delete filter.current;
+    delete filter.pageSize;
+    const offset = (+currentPage - 1) * +limit;
+    const defaultLimit = +limit ? +limit : 10;
+
+    const totalItems = (await this.permissionModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.permissionModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .exec();
+
+    return {
+      meta: {
+        currentPage, //trang hiện tại
+        pageSize: defaultLimit, //số lượng phần tử trên 1 trang
+        totalPages, //tổng số trang
+        totalItems, //tổng số phần tử
+      },
+      result,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} permission`;
+  async findOne(id: string) {
+    try {
+      const permission = await this.permissionModel.findById(id);
+      return permission;
+    } catch (error) {
+      return 'Permission not found';
+    }
+  }
+  async update(
+    id: string,
+    updatePermissionDto: UpdatePermissionDto,
+    user: IUser,
+  ) {
+    const permission = await this.permissionModel.updateOne(
+      {
+        _id: id,
+      },
+      {
+        ...updatePermissionDto,
+        updatedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+    return permission;
   }
 
-  update(id: number, updatePermissionDto: UpdatePermissionDto) {
-    return `This action updates a #${id} permission`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} permission`;
+  async remove(id: string, user: IUser) {
+    try {
+      await this.permissionModel.updateOne(
+        {
+          _id: id,
+        },
+        {
+          deletedBy: {
+            _id: user._id,
+            email: user.email,
+          },
+        },
+      );
+      return await this.permissionModel.softDelete({ _id: id });
+    } catch (error) {
+      return 'Permission not found';
+    }
   }
 }
