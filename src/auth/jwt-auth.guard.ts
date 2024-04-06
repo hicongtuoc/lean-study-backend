24f62975a63f856b4132e29275486b578b7e1ad6
Long Lê Đăng <hicongtuoc@gmail.com>
2024-04-07 02:17:57 +0700

    add role guard

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,11 +1,13 @@
 import {
   ExecutionContext,
+  ForbiddenException,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IS_PUBLIC_KEY } from '../decorator/customize';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -24,10 +26,25 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     return super.canActivate(context);
   }
 
-  handleRequest(err, user, info) {
+  handleRequest(err, user, info, context: ExecutionContext) {
+    const request: Request = context.switchToHttp().getRequest();
     // You can throw an exception based on either "info" or "err" arguments
     if (err || !user) {
       throw err || new UnauthorizedException('Token không hợp lệ');
+    }
+
+    //check permission
+    const targetMethod = request.method;
+    const targetEndPoint = request.route?.path;
+
+    const userPermissions = user?.permissions ?? [];
+    const isExist = userPermissions.find(
+      (permission) =>
+        targetMethod === permission.method &&
+        targetEndPoint === permission.apiPath,
+    );
+    if (!isExist) {
+      throw new ForbiddenException('Không có quyền truy cập');
     }
     return user;
   }
